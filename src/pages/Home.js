8320e1b275ef85624152540883b04970c1d7afce
digitinok <75326757+digitinok@users.noreply.github.com>
2023-03-17 21:42:56 +0000

    updated brand statements, project descriptions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,10 @@ function Home() {
         <div className="col-sm text-start">
           <h2 className="my-4">Konstanze Bartsch</h2>
           <h4 className="mb-3"> Software Engineer</h4>
-          <p> I'm a Software Engineer working in the automotive industry.</p>
+          <p> Readable, maintainable code, tested to fulfil expected / safety standards
+             </p>
+          <h4 className="mb-3 mt-4">Skills</h4>
+          <p>HTML, CSS, JavaScipt, React, Python, Matlab/Simulink</p>
         </div>
       </div>
     </div>
